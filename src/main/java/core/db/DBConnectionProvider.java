b32629c5b93369a12aa15dd09ca54f99630c863c
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-09-14 16:08:04 +0400

    initial commit

--- src/main/java/core/db/DBConnectionProvider.java
+++ src/main/java/core/db/DBConnectionProvider.java
@@ -1,11 +1,39 @@
 package core.db;
 
-public class DatabaseConnectionProvider {
-    private DatabaseConnectionProvider() {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionProvider {
+    private final static DBConnectionProvider INSTANCE = new DBConnectionProvider();
+    private Connection connection;
+
+    private static final String DB_URL = "jdbc:mysql://sql6.freesqldatabase.com:3306/sql6519582";
+    private static final String DB_USERNAME = "sql6519582";
+    private static final String DB_PASSWORD = "PNHwRsSljD";
+
+    private DBConnectionProvider() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static DBConnectionProvider getINSTANCE() {
+        var x = INSTANCE;
+        System.out.println(x);
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
--- src/main/java/server/servlet/UserServlet.java
+++ src/main/java/server/servlet/UserServlet.java
@@ -1,2 +1,23 @@
-package server.servlet;public class UserServlet {
+package server.servlet;
+
+
+import java.sql.Connection;
+import core.db.DBConnectionProvider;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/")
+public class UserServlet extends HttpServlet {
+//    private Connection connection = DBConnectionProvider.getINSTANCE().getConnection();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = DBConnectionProvider.getINSTANCE().getConnection();
+        System.out.println(connection);
+        req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
+    }
 }
